55668f9baf51b121021c2ec7ffe18f61f81b8e8b
Xmal Morthen <xmal.morthen@gmail.com>
2019-03-20 23:36:23 -0600

    Signed-off-by: Xmal Morthen <xmal.morthen@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { TramitesServiciosComponent } from './pages/tramites-servicios/tramites-servicios.component';
 import { ContactanosComponent } from './pages/contactanos/contactanos.component';
 import { UbicanosComponent } from './pages/ubicanos/ubicanos.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,9 +25,17 @@ import { UbicanosComponent } from './pages/ubicanos/ubicanos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDxWmnsWgwatmiF98NOAEbaVHVP_9PPdOc'
+    }),
+    AgmSnazzyInfoWindowModule
   ],
-  providers: [],
+  providers: [
+    GoogleMapsAPIWrapper
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/ubicanos/ubicanos.component.ts
+++ src/app/pages/ubicanos/ubicanos.component.ts
@@ -1,15 +1,118 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild, NgZone, OnInit } from "@angular/core";
+import { MapsAPILoader, AgmMap } from "@agm/core";
+import { GoogleMapsAPIWrapper } from "@agm/core/services";
+
+declare var google: any;
+
+interface MarkerInfo {
+  img?: string;
+  info?: string;
+}
+
+interface Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+  isOpen? : boolean;
+  info: MarkerInfo;
+}
+
+interface Location {
+  lat: number;
+  lng: number;
+  viewport?: Object;
+  zoom: number;
+  address_level_1?: string;
+  address_level_2?: string;
+  address_country?: string;
+  address_zip?: string;
+  address_state?: string;
+  markers?: Marker[];
+}
 
 @Component({
-  selector: 'app-ubicanos',
-  templateUrl: './ubicanos.component.html',
-  styleUrls: ['./ubicanos.component.css']
+  selector: "app-ubicanos",
+  templateUrl: "./ubicanos.component.html",
+  styleUrls: ["./ubicanos.component.css"]
 })
 export class UbicanosComponent implements OnInit {
+  geocoder: any;
+  //@ViewChild(AgmMap) map: AgmMap;
+  infoWindowOpened = null;
+  previous_info_window = null;
 
-  constructor() { }
+  public location: Location = {
+    lat: 19.2433,
+    lng: -103.725,
+    markers: [],
+    zoom: 10
+  };
 
-  ngOnInit() {
+  constructor(
+    public mapsApiLoader: MapsAPILoader,
+    private zone: NgZone,
+    private wrapper: GoogleMapsAPIWrapper
+  ) {
+    this.mapsApiLoader = mapsApiLoader;
+    this.zone = zone;
+    this.wrapper = wrapper;
+    this.mapsApiLoader.load().then(() => {
+      this.location.markers = [
+        {
+          lat: 19.2433,
+          lng: -103.725,
+          label: "Colima",
+          draggable: true,
+          info: {
+            info: "Información Kiosco Colima"
+          }
+        },
+        {
+          lat: 19.39444,
+          lng: -103.73056,
+          label: "Comala",
+          draggable: false,
+          info: {
+            info: "Información Kiosco Comala"
+          }
+        },
+        {
+          lat: 19.05922,
+          lng: -104.30156,
+          label: "Manzanillo",
+          draggable: true,
+          info: {
+            info: "Información Kiosco Manzanillo"
+          }
+        }
+      ];
+
+      this.geocoder = new google.maps.Geocoder();
+    });
   }
 
+  ngOnInit() {}
+
+  close_window() : void {
+    if (this.previous_info_window != null) {
+      this.previous_info_window.close();
+    }
+  }
+
+  select_marker(infoWindow) : void {
+    if (this.previous_info_window == null)
+      this.previous_info_window = infoWindow;
+    else {
+      this.infoWindowOpened = infoWindow;
+      this.previous_info_window.close();
+    }
+    this.previous_info_window = infoWindow;
+  }
+
+  public btnClick(event: Event) : void{
+    this.location.markers[2].isOpen = true;
+    this.location.lat = this.location.markers[2].lat;
+    this.location.lng = this.location.markers[2].lng;
+  }
 }
